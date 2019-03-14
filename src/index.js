731c180d0fb940ee275515be14039902fb4970a9
Anabfmv <anastasia.yarashukbfmv@gmail.com>
2019-03-15 01:08:04 +0300

    task advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
-module.exports = function getZerosCount(number, base) {
-  let del = maxnod(base);
-  let be = counter(number,del);
-
-  return be;
-
+module.exports = 
+function getZerosCount(number, base) {
+  let del = primeDivider(base);
+    if(del.length==0){
+  del.push(base);
 }
 
-function maxnod (n){
-  let del = 2, toto = Math.sqrt(n)+1
+  let res = Array.from(new Set(del)).map(function(x){
+    let col = del.filter(function(op){return op==x}).length;
+    return Math.floor(counter(number,x)/col)
+  });
+  let min = Math.min.apply(null, res);
+  
+return min;
+ }
+
+function primeDivider (n){
+  let list=[];
+  let del = 2, toto = n/2
   while(del <= toto)
   if(n%del == 0)
   {
-    max = del;
+    if(isPrime(del)){
+      list.push(del);
+    }
     n = n/del;
   }
 else
 ++del;
+return list;
+}
 
-if(n>1)
-max = Math.max(max, n)
-return max;
+function isPrime (num){
+    for(let i=2; i<num; i++){
+      if(num%i==0){
+        return false;
+      }
+    }
+  return true;
 }
 
 function counter (number,del){
@@ -30,7 +47,7 @@ function counter (number,del){
     colNULL = Math.floor(number / Math.pow(del,i));
     t += colNULL;
     i++;
-  } while (colNULL !=0);
-
+  }
+  while (colNULL !=0);
   return t;
 }
